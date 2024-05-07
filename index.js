142a9229e03a3d02774cc66a4eea09b5624ecc14
alvinanulat <54806040+alvinanulat@users.noreply.github.com>
2024-05-07 13:23:11 +0800

    keys

--- index.js
+++ index.js
@@ -198,9 +198,8 @@ app.post("/api/data/", (req, res) => {
   //console.log("Received data:", receivedData2);
   res.status(200).send("Data received successfully");
   console.log(receivedData2);
-  const invalidJson = receivedData2;
-  const fixedJson = fixString(invalidJson);
-  console.log(fixedJson);
+  const keys = Object.keys(receivedData2);
+  console.log(keys[0]);
   const pingValue = receivedData2.internet.ping;
   var pingstate = receivedData2.internet.pingstate;
   const hotspotcount = receivedData2.hotspot.hotspotcount;
